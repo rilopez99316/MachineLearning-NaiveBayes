6bb5fafd8e57bafcb823e3f0fe2e89ca8b887ba1
rilopez99316 <ilbroberto99316@gmail.com>
2022-04-04 21:38:27 -0600

    turn data to binary

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -4,3 +4,9 @@ import numpy as np
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 x_train = np.float32(x_train/255).reshape(x_train.shape[0],-1)
 x_test = np.float32(x_test/255).reshape(x_test.shape[0],-1)
+
+mean = np.mean(x_train)
+x_train_binary = (x_train > mean).astype(int) #record boolean: as 1 or 0 and sets it to x_train_binary
+x_test_binary = (x_test > mean).astype(int)#record boolean: as 1 or 0 and sets it to x_test_binary
+
+print(x_train[0])
