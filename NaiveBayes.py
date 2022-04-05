8cd8c0b6166b3fc405a0a7af87a7d5e51ee91a1c
rilopez99316 <ilbroberto99316@gmail.com>
2022-04-04 21:41:40 -0600

    classify test data

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 x_train = np.float32(x_train/255).reshape(x_train.shape[0],-1)
@@ -23,3 +26,25 @@ for i in range(10):
   p_att_given_class[i] = np.mean(x_train_binary[index], axis = 0) #calculates average of the 1's in each clss, for only column 0
 
 # the number of times attribute j is equal to 1 in training instances of class j / over the number of training instances of class j
+
+def classify(x_test, p_class, p_att_given_class):
+    prediction =  np.zeros(x_test.shape[0])
+    probabilities =  np.zeros((x_test.shape[0],10))
+
+    for i,image in enumerate(x_test):
+        p = (p_att_given_class * image) + (1 - p_att_given_class) * (1 - image) #p = x*pac + (1-x)*(1-pac)
+        m = np.prod(p, axis = 1) #  p = np.prod(p, axis=1)
+        probabilities[i] = m * p_class
+
+    prediction = np.argmax(probabilities, axis = 1) 
+    return prediction
+
+prediction = classify(x_test_binary, p_class, p_att_given_class)
+
+def accuracy(Y_test_binary, pred):
+  return np.sum(Y_test_binary == pred) / len(Y_test_binary)
+
+print("accuracy: ", accuracy(y_test, prediction))
+cm = confusion_matrix(y_test, prediction)
+ConfusionMatrixDisplay(confusion_matrix=cm).plot()
+plt.show()
